38937506e49dc8d8c1388869bc57d2dcbff976e6
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-17 02:17:03 -0500

    Run unit tests automatically on build

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
-cd build
-cmake ..
+BUILD_DIR=build
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+cmake -Dtest=ON ..
 make
+make test
+cd ..
+$BUILD_DIR/cppproperties_unittests
